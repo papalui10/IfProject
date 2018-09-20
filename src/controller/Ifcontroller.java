3f99c8715b1afb8e8c93a55b1d4e354e6c350903
Luis Veliz <luisvelizjr@gmail.com>
2018-09-20 13:33:34 -0600

    added user interface

--- src/controller/Ifcontroller.java
+++ src/controller/Ifcontroller.java
@@ -2,17 +2,18 @@ package controller;
 
 //Import Section
 import model.Computers;
+
 import javax.swing.JOptionPane;
 
-
-public class Ifcontroller userComputer;
+public class Ifcontroller
 {
+	private Computers userComputer;
 	/**
 	 * Builds the instance Controller
 	 */
 	public Ifcontroller()
 	{
-		userComputer = new Computer();
+		userComputer = new Computers();
 	}
 	
 	/**
@@ -20,6 +21,39 @@ public class Ifcontroller userComputer;
 	 */
 	public void start()
 	{
+		String userInput = JOptionPane.showInputDialog("Enter your favorite computer brand");
+		userComputer.setBrandName(userInput);
 		
+		userInput = JOptionPane.showInputDialog("Enter your price");
+		int price = -99;
+		if(validInt(userInput))
+		{
+			price = Integer.parseInt(userInput);
+		}
+		userComputer.setPrice(price);
+		
+		userInput = JOptionPane.showInputDialog("Enter the games for your computer");
+		userComputer.setGames(userInput);
+		
+		JOptionPane.showMessageDialog(null, "Your favorite computer brand is" + userComputer.getBrandName() + "!");
+		JOptionPane.showMessageDialog(null, "Your budget is" + userComputer.getPrice() + "!");
+		JOptionPane.showMessageDialog(null, "Your games are" + userComputer.getGames());
+	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You need to type in a whole number :D");
+		}
+		
+		return isValid;
 	}
 }
