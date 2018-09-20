f0086b8af3cf9f5d028b0ca8b738c0e050a7e633
Luis Veliz <luisvelizjr@gmail.com>
2018-09-20 12:03:14 -0600

    made constructors

--- src/model/Computers.java
+++ src/model/Computers.java
@@ -2,5 +2,18 @@ package model;
 
 public class Computers
 {
+	//Declaration section
+	//Data member section
+	//Also called instance variables
+	//These are special - they get a "default" value
+	//All numbers => 0
+	//boolean => false
+	//Objects => null!!!
+	
+	private String brand;
+	private int price;
+	private String games;
+	
+	public 
 
 }
